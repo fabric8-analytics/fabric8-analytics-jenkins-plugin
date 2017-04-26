72e431c186ebd2d406d608c675a2a97f252c10e5
Michal Srb <michal@redhat.com>
2017-04-26 17:44:43 +0200

    Switch to gson

--- src/main/java/com/redhat/jenkins/plugins/bayesian/Bayesian.java
+++ src/main/java/com/redhat/jenkins/plugins/bayesian/Bayesian.java
@@ -2,8 +2,11 @@ package com.redhat.jenkins.plugins.bayesian;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.Charset;
 import java.util.Collection;
 import java.util.List;
 
@@ -15,10 +18,11 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import org.apache.http.protocol.HTTP;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.io.ByteStreams;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import hudson.FilePath;
 
@@ -71,27 +75,26 @@ import hudson.FilePath;
         builder = null;
         httpPost.setEntity(multipart);
 
-        ObjectMapper mapper = null;
         BayesianResponse responseObj = null;
         try (CloseableHttpClient client = HttpClients.createDefault(); CloseableHttpResponse response = client.execute(httpPost)) {
             HttpEntity entity = response.getEntity();
-            String responseContent = entity != null ? EntityUtils.toString(entity) : null;
             // Yeah, the endpoint actually returns 200 from some reason;
             // I wonder what happened to the good old-fashioned 202 :)
             if (response.getStatusLine().getStatusCode() != 200) {
-                throw new BayesianException("Bayesian error: " + responseContent);
+                throw new BayesianException("Bayesian error: " + response.getStatusLine().getStatusCode());
             }
 
-            mapper = new ObjectMapper();
-            responseObj = mapper.readValue(responseContent, BayesianResponse.class);
-            String analysisUrl = stackAnalysesUrl + "/" + responseObj.getId();
-            BayesianStepResponse bayesianStepResponse = new BayesianStepResponse(responseObj.getId(), "", analysisUrl, true);
-            return bayesianStepResponse;
+            Charset charset = ContentType.get(entity).getCharset();
+            try (InputStream is = entity.getContent(); Reader reader = new InputStreamReader(is, charset != null ? charset : HTTP.DEF_CONTENT_CHARSET)) {
+                Gson gson = new GsonBuilder().create();
+                responseObj = gson.fromJson(reader, BayesianResponse.class);
+                String analysisUrl = stackAnalysesUrl + "/" + responseObj.getId();
+                return new BayesianStepResponse(responseObj.getId(), "", analysisUrl, true);
+            }
         } catch (IOException e) {
             throw new BayesianException("Bayesian error", e);
         } finally {
             // just to be sure...
-            mapper = null;
             responseObj = null;
             httpPost = null;
             multipart = null;
