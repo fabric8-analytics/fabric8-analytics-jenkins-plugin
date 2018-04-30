6d5ddccc778d4653fe9f1115c5e232785cc87d6c
root <yzainee@redhat.com>
2018-04-30 16:43:19 +0530

    #845 Added logic to send email in the header for analytics

--- src/main/java/com/redhat/jenkins/plugins/bayesian/User.java
+++ src/main/java/com/redhat/jenkins/plugins/bayesian/User.java
@@ -63,14 +63,4 @@ public class User {
 		}
 		
 	}
-
-	public static void main(String[] args) {
-		
-		System.out.println("Hello");
-		try {
-		System.out.println("Hello:" + new Bayesian().getEmail() );
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 }
