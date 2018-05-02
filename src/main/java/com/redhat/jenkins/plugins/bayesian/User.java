52c3d98313e37db7b81169ea8896e194549c1a65
root <yzainee@redhat.com>
2018-05-02 12:21:12 +0530

    removed unwanted imports

--- src/main/java/com/redhat/jenkins/plugins/bayesian/User.java
+++ src/main/java/com/redhat/jenkins/plugins/bayesian/User.java
@@ -1,24 +1,7 @@
 package com.redhat.jenkins.plugins.bayesian;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.charset.Charset;
 import java.util.List;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.entity.ContentType;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.protocol.HTTP;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 
 public class User {
